37d4c329551ddd47e57bfdef1d55ded3da148f2a
Rratic <panjiacheng@aliyun.com>
2021-04-04 23:16:45 +0800

    文档更新

--- src/core.cpp
+++ src/core.cpp
@@ -3,3 +3,15 @@ luogu_stack Three[3];
 uint32_t state_num,default_state;
 vector<order>orders;
 string public_message;
+bool initall(queue<string>s){
+    vector<string>t=split(s.front(),' ');
+    state_num=s_to_i32(t[0]),default_state=s_to_i32(t[1]);
+    s.pop();
+    while(!s.empty()){
+        order tt;
+        tt.load(s.front());
+        s.pop();
+        orders.push_back(tt);
+    }
+    return true;
+}
