a0d34e7c10c45a221ce7d89d9d4cb7b1288421fb
Rratic <panjiacheng@aliyun.com>
2021-04-04 21:11:17 +0800

    基本类

--- src/base.cpp
+++ src/base.cpp
@@ -1,1 +1,19 @@
 #include "base.h"
+uint16_t MAX_STATE=100000,MAX_STACK=1000000;
+uint32_t do_mod_p=(1<<23)+1;
+vector<string> split(string s,char sign){
+    vector<string>ans;
+    size_t l=s.length();
+    string temp;
+    for(size_t i=0;i<l;++i){
+        if(s[i]==sign){
+            if(temp!=""){
+                ans.push_back(temp);
+                temp="";
+            }
+        }
+        else temp+=s[i];
+    }
+    if(temp!="")ans.push_back(temp);
+    return ans;
+}
---
+++ src/base.h
@@ -0,0 +1,20 @@
+#include <stack>
+#include <vector>
+#include <string>
+#include <stdint.h>
+using std::stack;
+using std::vector;
+using std::string;
+extern uint16_t MAX_STATE,MAX_STACK;
+extern uint32_t do_mod_p;
+vector<string> split(string s,char sign);
+class luogu_stack{
+    stack<uint32_t>data;
+    uint16_t size;
+    luogu_stack();
+}Three[3];
+class order{
+    uint8_t name;
+    uint32_t args[4];
+    bool load(string s);
+};
